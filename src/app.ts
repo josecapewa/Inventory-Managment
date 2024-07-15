b90ec8e6aace28e0284086db41aac4441639e584
José Capewa <capewajose@gmail.com>
2024-07-15 15:38:00 +0000

    chore: Update npm dependencies to include Prisma, TypeScript, and dotenv updates

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import fastify from 'fastify';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = fastify({
     logger: true
@@ -8,7 +11,8 @@ app.get('/', async (request, reply) => {
     return "hello world ";
 });
 
-app.listen({ port: 3303 }, (err, address) => {
+
+app.listen({ port: Number(process.env.PORT) }, (err, address) => {
     if (err) {
         app.log.error(err);
         process.exit(1);
