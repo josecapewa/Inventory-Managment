aeb3997e9b7fc6a0cfdcf3d495e33d6cd23e1afd
José Capewa <capewajose@gmail.com>
2024-07-19 22:19:59 +0000

    feat: Refactor createProduct route to handle errors and improve response handling

--- src/routes/products/products.routes.ts
+++ src/routes/products/products.routes.ts
@@ -32,37 +32,47 @@ export async function createProduct(fastify: FastifyInstance) {
 
     fastify.get(
         '/product/:id'
-        , async (request: FastifyRequest<{ Params: { id: string } }>) => {
-            const { id } = request.params;
+        , async (request: FastifyRequest<{ Params: { id: string } }>, response: FastifyReply) => {
+            try{
+                const { id } = request.params;
 
-            const product = await prisma.products.findUnique({
-                where: {
-                    id: id
-                }
-            });
+                const product = await prisma.products.findUnique({
+                    where: {
+                        id: id
+                    }
+                });
 
-            return product;
+                return response.code(200).send(product);
+            } catch(e){
+                console.log(e);
+                return response.code(500).send({ message: "Internal Server Error" });
+            }
 
         }
     )
     fastify.put(
         "/product/update/:id",
-        async (request: FastifyRequest<{ Params: { id: string }, Body: Product }>) => {
-            const { id } = request.params;
-            const { name, categoryId, mediaId } = request.body;
-
-            const product = await prisma.products.update({
-                where: {
-                    id: id
-                },
-                data: {
-                    name,
-                    categoryId,
-                    mediaId
-                }
-            });
-
-            return product;
+        async (request: FastifyRequest<{ Params: { id: string }, Body: Product }>, response: FastifyReply) => {
+            try{
+                const { id } = request.params;
+                const { name, categoryId, mediaId } = request.body;
+    
+                const product = await prisma.products.update({
+                    where: {
+                        id: id
+                    },
+                    data: {
+                        name,
+                        categoryId,
+                        mediaId
+                    }
+                });
+    
+                return response.code(200).send(product);
+            } catch(e){
+                console.log(e);
+                return response.code(500).send({ message: "Internal Server Error" });
+            }
         }
     )
 }
